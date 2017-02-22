9c084001d7bf8f2a5a51adad5cdc6939698fc240
Jonas R. Hartogsohn <Jonas.Rasborg@Gmail.com>
2017-02-22 17:13:03 +0100

    Agents

---
+++ AgentAssignment/AgentAssignment/Agents.cs
@@ -0,0 +1,161 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using MvvmFoundation.Wpf;
+
+namespace I4GUI
+{
+    public class Agents : ObservableCollection<Agent>, INotifyPropertyChanged
+    {
+        OpenSave openSave;
+        public Agents()
+        {
+            Add(new Agent("007", "Bond, James", "London", "Killing", "Kill Trump"));
+            Add(new Agent("006", "Berry, Halle", "London", "Spy", "Eat lunch"));
+
+            openSave = new OpenSave();
+        }
+
+
+        #region Commands
+
+        ICommand _saveCommand;
+        public ICommand SaveCommand
+        {
+            get { return _saveCommand ?? (_saveCommand = new RelayCommand(SaveAgents)); }
+        }
+
+        private void SaveAgents()
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+            dlg.ShowDialog();
+
+            dlg.DefaultExt = ".xml";
+
+            openSave.saveAgents(this, dlg.FileName);
+        }
+
+        ICommand _loadCommand;
+        public ICommand LoadCommand
+        {
+            get { return _loadCommand ?? (_loadCommand = new RelayCommand(LoadAgents)); }
+        }
+
+        private void LoadAgents()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.ShowDialog();
+
+            this.ClearItems();
+
+            Agents agenter = openSave.readAgents(dlg.FileName);
+
+            foreach (var item in agenter)
+            {
+                Add(item);
+            }
+        }
+
+
+        ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get { return _addCommand ?? (_addCommand = new RelayCommand(AddAgent)); }
+        }
+
+        private void AddAgent()
+
+        {
+            Add(new Agent());
+            CurrentIndex = Count - 1;
+        }
+
+
+        ICommand _deleteCommand;
+        public ICommand DeleteCommand
+        {
+            get { return _deleteCommand ?? (_deleteCommand = new RelayCommand(DeleteAgent, DeleteAgent_CanExecute)); }
+        }
+
+        private void DeleteAgent()
+        {
+            RemoveAt(CurrentIndex);
+        }
+
+        private bool DeleteAgent_CanExecute()
+        {
+            if (Count > 0 && CurrentIndex >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        ICommand _nextCommand;
+        public ICommand NextCommand
+        {
+            get
+            {
+                return _nextCommand ?? (_nextCommand = new RelayCommand(
+                    () => ++CurrentIndex,
+                    () => CurrentIndex < (Count - 1)));
+            }
+        }
+
+        ICommand _PreviusCommand;
+        public ICommand PreviusCommand
+        {
+            get { return _PreviusCommand ?? (_PreviusCommand = new RelayCommand(PreviusCommandExecute, PreviusCommandCanExecute)); }
+        }
+
+        private void PreviusCommandExecute()
+        {
+            if (CurrentIndex > 0)
+                --CurrentIndex;
+        }
+
+        private bool PreviusCommandCanExecute()
+        {
+            if (CurrentIndex > 0)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion // Commands
+
+        #region Properties
+
+        int currentIndex = -1;
+
+        public int CurrentIndex
+        {
+            get { return currentIndex; }
+            set
+            {
+                if (currentIndex != value)
+                {
+                    currentIndex = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ AgentAssignment/AgentAssignment/OpenSave.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace I4GUI
+{
+    class OpenSave
+    {
+        private Agents agenter;
+
+        public Agents readAgents(string path) // Brugt i Undervisning ved JRT: Construction
+        {
+            string mypath = @"" + path;
+
+            using (FileStream xmlStream = new FileStream(mypath, FileMode.Open))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(xmlStream))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(Agents));
+
+                    Agents deserializedMembers = serializer.Deserialize(xmlReader) as Agents;
+
+                    agenter = deserializedMembers;
+
+                    xmlReader.Close();
+
+                    return agenter;
+                }
+            }
+        }
+         
+
+
+        public void saveAgents(Agents agenter, string path) // Brugt i Undervisning ved JRT: Construction
+        {
+            string mypath = @"" + path + ".xml";
+
+            using (FileStream xmlStream = new FileStream(mypath, FileMode.OpenOrCreate))
+            {
+
+                using (XmlWriter xmlWriter = XmlWriter.Create(xmlStream))
+
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(Agents));
+
+                    serializer.Serialize(xmlWriter, agenter);
+                    xmlWriter.Flush();
+                    xmlWriter.Close();
+                }
+            }
+        }
+    }
+}
